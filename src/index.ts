c37b0bca8b56173b01b102d7e3b902382f774d46
Aliibrahim4p <167108686+Aliibrahim4p@users.noreply.github.com>
2026-04-20 10:37:51 +0300

    Added Cake Builder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import logger from './util/logger';
-import { readCSVFile } from './util/parser';
-
+import { Cake } from './model/Cake.model';
+import { CakeBuilder } from './model/builders/Cake.builder';
 async function main() {
-  const data = await readCSVFile('./src/data/cake orders.csv', true);
-  // for each data row log the row
-  data.forEach((row) => logger.info(row));
+  const cakeBuilder = new CakeBuilder();
+  const cake = cakeBuilder
+    .setType('Birthday')
+    .setFlavor('Chocolate')
+    .setAllergies('None')
+    .setFilling('Vanilla')
+    .setSize(8)
+    .setLayers(3)
+    .setFrostingType('Buttercream')
+    .setFrostingFlavor('Vanilla')
+    .setDecorationType('Sprinkles')
+    .setDecorationColor('Red')
+    .setCustomMessage('Happy Birthday')
+    .setShape('Round')
+    .setSpecialIngredients('None')
+    .setPackagingType('Box')
+    .setPrice(45.99)
+    .setQuantity(1)
+    .build();
+  console.log(cake);
 }
 main();
