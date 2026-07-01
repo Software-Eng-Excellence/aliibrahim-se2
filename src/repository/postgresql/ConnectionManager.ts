538da88787ec6e5589c4aa1512b1e520aa031075
Ali Ibrahim <aliibrahim4p@gmail.com>
2026-07-01 03:25:21 +0300

    setup postgre

---
+++ src/repository/postgresql/ConnectionManager.ts
@@ -0,0 +1,25 @@
+import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export class ConnectionManager {
+  private static pool: Pool;
+  public static getPool(): Pool {
+    if (!this.pool) {
+      if (!process.env.DATABASE_URL) {
+        throw new Error('DATABASE_URL environment variable is not defined');
+      }
+      this.pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        max: 10,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+      });
+    }
+    return this.pool;
+  }
+}
